723a6107b1083ee492ddfa36e4aa83524a703b70
Mehdi Fathi <armenarmen125@gmail.com>
2025-10-24 07:03:26 +0330

    8and9Debuging

--- 8.py
+++ 8.py
@@ -1,60 +1,38 @@
-
-import pandas as pd
-import datamol as dm
-from tqdm.auto import tqdm
+from rdkit import Chem
+from rdkit.Chem.Draw import MolsToGridImage
 from rdkit.Chem import rdDepictor
+import pandas as pd
+from rdkit.Chem import EnumerateStereoisomers
+from rdkit.Chem.MolStandardize import rdMolStandardize
+import subprocess , os
+
 import mols2grid
 
 
-
 rdDepictor.SetPreferCoordGen(True)
-pd.options.display.float_format = '{:,.2f}'.format
 
-url = "https://raw.githubusercontent.com/PatWalters/yamc/main/data/HERG.smi"
-df = pd.read_csv(url,sep=" ",names=["SMILES","Name","pIC50"])
-
-# print(df.head())
-
-
-df['IC50'] = dm.molar.log_to_molar(df.pIC50,'uM')
+smi_1 = "F[C@](Cl)(Br)c1ccc(cc1)C(F)(Cl)Br"
+mol_1 = Chem.MolFromSmiles(smi_1)
+smi_2 = "F[C@@](Cl)(Br)c1ccc(cc1)C(F)(Cl)Br"
+mol_2 = Chem.MolFromSmiles(smi_2)
 
 
 
-df['mol'] = dm.from_df(df, smiles_column="SMILES")
+mol_list = [mol_1, mol_2]
 
 
-def max_ring_size(mol):
-    """Get the size of the largest ring in a molecule
 
-    :param mol: input_molecule
-    :return: size of the largest ring or 0 for an acyclic molecule
-    """
-    ri = mol.GetRingInfo()
-    atom_rings = ri.AtomRings()
-    if len(atom_rings) == 0:
-        return 0
-    else:
-        return max([len(x) for x in ri.AtomRings()])
-    
-    
-my_prop_dict = {
-    "mw" : dm.descriptors.mw,
-    "logp" : dm.descriptors.clogp,
-    "hbd" : dm.descriptors.n_lipinski_hbd,
-    "hba" : dm.descriptors.n_lipinski_hba,
-    "max_ring_size" : max_ring_size
+data = {
+    'Molecule': [mol_1, mol_2],
+    'Name': ['Stereoisomer 1', 'Stereoisomer 2'],
+    'SMILES': [smi_1, smi_2]
 }
+df = pd.DataFrame(data)
 
+grid = mols2grid.MolGrid(df, subset=["img", "Name", "SMILES"])
+grid.save("two_grid.html")
 
+print("Grid saved as 'two_grid.html'")
+# Open in default browser
+subprocess.run(["xdg-open", "two_grid.html"])
 
-prop_df = dm.descriptors.batch_compute_many_descriptors(df.mol,properties_fn=my_prop_dict,add_properties=False, progress=True)
-# print(prop_df)
-
-df = pd.concat([df,prop_df],axis=1)
-
-print(df)
-
-
-
-df_ro5_ok = df.query("mw <= 500 and logp <= 5 and hbd <= 5 and hba <= 10")
-# print(len(df_ro5_ok))
---
+++ 9.py
@@ -0,0 +1,60 @@
+
+import pandas as pd
+import datamol as dm
+from tqdm.auto import tqdm
+from rdkit.Chem import rdDepictor
+import mols2grid
+
+
+
+rdDepictor.SetPreferCoordGen(True)
+pd.options.display.float_format = '{:,.2f}'.format
+
+url = "https://raw.githubusercontent.com/PatWalters/yamc/main/data/HERG.smi"
+df = pd.read_csv(url,sep=" ",names=["SMILES","Name","pIC50"])
+
+# print(df.head())
+
+
+df['IC50'] = dm.molar.log_to_molar(df.pIC50,'uM')
+
+
+
+df['mol'] = dm.from_df(df, smiles_column="SMILES")
+
+
+def max_ring_size(mol):
+    """Get the size of the largest ring in a molecule
+
+    :param mol: input_molecule
+    :return: size of the largest ring or 0 for an acyclic molecule
+    """
+    ri = mol.GetRingInfo()
+    atom_rings = ri.AtomRings()
+    if len(atom_rings) == 0:
+        return 0
+    else:
+        return max([len(x) for x in ri.AtomRings()])
+    
+    
+my_prop_dict = {
+    "mw" : dm.descriptors.mw,
+    "logp" : dm.descriptors.clogp,
+    "hbd" : dm.descriptors.n_lipinski_hbd,
+    "hba" : dm.descriptors.n_lipinski_hba,
+    "max_ring_size" : max_ring_size
+}
+
+
+
+prop_df = dm.descriptors.batch_compute_many_descriptors(df.mol,properties_fn=my_prop_dict,add_properties=False, progress=True)
+# print(prop_df)
+
+df = pd.concat([df,prop_df],axis=1)
+
+print(df)
+
+
+
+df_ro5_ok = df.query("mw <= 500 and logp <= 5 and hbd <= 5 and hba <= 10")
+# print(len(df_ro5_ok))
